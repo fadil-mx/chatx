f49bef70a4d40a10da0f636c5fb2e7f65453273e
fadil-mx <fadilshereefmx@gmail.com>
2025-09-17 12:53:14 +0000

    created a dockerfile and compose.yaml

--- lib/validator.ts
+++ lib/validator.ts
@@ -1,21 +1,22 @@
-import { z } from 'zod';
+import { z } from 'zod'
 
-export const UserSchemaValidator=z.object({
-    name:z.string().min(3, "Name is required"),
-    email:z.string().min(1, "Email is required").email("Invalid email format"),
-    password:z.string().min(5, "Password must be at least 5 characters long")
+export const UserSchemaValidator = z.object({
+  name: z.string().min(3, 'Name is required'),
+  email: z.string().min(1, 'Email is required').email('Invalid email format'),
+  password: z.string().min(5, 'Password must be at least 5 characters long'),
 })
 
-
-export const  UserSignInValidator = z.object({
-    email:z.string().min(1, "Email is required").email("Invalid email format"),
-    password:z.string().min(5, "Password must be at least 5 characters long")
+export const UserSignInValidator = z.object({
+  email: z.string().min(1, 'Email is required').email('Invalid email format'),
+  password: z.string().min(5, 'Password must be at least 5 characters long'),
 })
 
-export const UserSignUpValidator =UserSignInValidator.extend({
-    name:z.string().min(3, "Name is required"),
-    confirmPassword:z.string().min(5, "Confirm Password must be at least 5 characters long")
+export const UserSignUpValidator = UserSignInValidator.extend({
+  name: z.string().min(3, 'Name is required'),
+  confirmPassword: z
+    .string()
+    .min(5, 'Confirm Password must be at least 5 characters long'),
 }).refine((data) => data.password === data.confirmPassword, {
-    message: "Passwords do not match",
-    path: ["confirmPassword"]
+  message: 'Passwords do not match',
+  path: ['confirmPassword'],
 })
